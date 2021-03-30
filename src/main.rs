5a68267b4d7c5a2205a5bfd74a29e6e3aea7a3f7
Juuso Elo-Rauta <juuso.er@gmail.com>
2021-03-30 21:51:00 +0300

    remove unnecessary stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,2 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
-
-#[get["/"]]
-fn root() -> &'static str {
-    "ima root" 
-}
-
 fn main() {
-    rocket::ignite().mount("/", routes![root]).launch();
 }
